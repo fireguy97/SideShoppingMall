62eec06551025d465ec7caaab2e2585e2a596ff1
jipark96 <wlgus996@naver.com>
2023-05-16 18:58:37 +0900

    좋아요 수정

--- src/page/Best.js
+++ src/page/Best.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "../layout/Layout";
 import BestList from "../component/best/BestList";
+import items from "../db/items.json";
 
 const Best = () => {
   return (
     <Layout>
-      <BestList />
+      <BestList filteredItems={items} />
     </Layout>
   );
 };
